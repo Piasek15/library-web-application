214540487db17483a0c633d58476cc829773f3e2
Patrick <piasecki.patrick.15@gmail.com>
2018-04-26 10:56:18 +0200

    adding Book Builder design pattern and BookService tests

--- src/main/java/pl/piasecki/librarywebapplication/domain/Book.java
+++ src/main/java/pl/piasecki/librarywebapplication/domain/Book.java
@@ -19,11 +19,47 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String title;
     private String category;
     private Integer pages;
 
     @ManyToMany(mappedBy = "books")
     private Set<Author> authors = new HashSet<>();
+
+    private Book(final Builder builder){
+        this.title = builder.title;
+        this.category = builder.category;
+        this.pages = builder.pages;
+        this.id = builder.id;
+    }
+
+    public static class Builder{
+        private final String title;
+        private String category;
+        private Integer pages;
+        private Long id;
+
+        public Builder(final String title){
+            this.title = title;
+        }
+
+        public Builder category(String category){
+            this.category = category;
+            return this;
+        }
+
+        public Builder pages(Integer pages){
+            this.pages = pages;
+            return this;
+        }
+
+        public Builder id(Long id){
+            this.id = id;
+            return this;
+        }
+
+        public Book build(){
+            return new Book(this);
+        }
+    }
 }
--- src/test/java/pl/piasecki/librarywebapplication/services/BookServiceTest.java
+++ src/test/java/pl/piasecki/librarywebapplication/services/BookServiceTest.java
@@ -6,19 +6,31 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import pl.piasecki.librarywebapplication.DTOs.mapper.BookMapper;
 import pl.piasecki.librarywebapplication.DTOs.model.BookDTO;
+import pl.piasecki.librarywebapplication.DTOs.model.BookPureDTO;
 import pl.piasecki.librarywebapplication.domain.Book;
 import pl.piasecki.librarywebapplication.repositories.BookRepository;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
  * Created by Patrick on 25 kwi 2018
  */
 public class BookServiceTest {
+    private static final String TITLE = "LOTR";
+    private static final String CATEGORY = "AAA";
+    private static final Integer PAGES = 111;
+    private static final long ID = 1L;
+
+
     BookService bookService;
 
     @Mock
@@ -40,5 +52,55 @@ public class BookServiceTest {
         assertEquals(2, bookDTOS.size());
     }
 
+    @Test
+    public void getBookById() {
+        Book book = new Book.Builder(TITLE)
+                .id(ID)
+                .pages(PAGES)
+                .category(CATEGORY)
+                .build();
 
+        when(bookRepository.findById(anyLong())).thenReturn(Optional.ofNullable(book));
+
+        BookDTO bookDTO = bookService.getBookById(ID);
+
+        assertEquals(TITLE, bookDTO.getTitle());
+        assertEquals(PAGES, bookDTO.getPages());
+    }
+
+
+    @Test
+    public void createBook() {
+        BookPureDTO bookPureDTO = new BookPureDTO();
+        bookPureDTO.setTitle(TITLE);
+
+        Book book = new Book.Builder(bookPureDTO.getTitle()).build();
+
+        when(bookRepository.save(any(Book.class))).thenReturn(book);
+
+        BookPureDTO savedDTO = bookService.createBook(bookPureDTO);
+
+        assertEquals(bookPureDTO.getTitle(), savedDTO.getTitle());
+    }
+
+    @Test
+    public void updateBook() {
+        BookPureDTO bookPureDTO = new BookPureDTO();
+        bookPureDTO.setTitle(TITLE);
+
+        Book book = new Book.Builder(bookPureDTO.getTitle()).build();
+
+        when(bookRepository.save(any(Book.class))).thenReturn(book);
+
+        BookPureDTO savedDTO = bookService.updateBook(anyLong(), bookPureDTO);
+
+        assertEquals(TITLE, savedDTO.getTitle());
+    }
+
+    @Test
+    public void deleteBook() {
+        bookService.deleteBook(ID);
+
+        verify(bookRepository, times(1)).deleteById(anyLong());
+    }
 }

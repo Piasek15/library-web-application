db9cb8ec7e726723965e4b3d5fb89e7f9cfd9e04
Patrick <piasecki.patrick.15@gmail.com>
2018-04-27 13:55:05 +0200

    adding simple spring security

---
+++ src/main/java/pl/piasecki/librarywebapplication/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package pl.piasecki.librarywebapplication.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by Patrick on 27 kwi 2018
+ */
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/books/**").hasRole("USER")
+                .antMatchers(HttpMethod.GET, "/authors/**").hasRole("USER")
+                .antMatchers("/**").hasRole("ADMIN")
+                .and()
+                .csrf().disable()
+                .httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}user").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}admin").roles("USER", "ADMIN");
+    }
+}
